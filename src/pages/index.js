b2ae19ab99260f00623e302b5b1849dae4725139
Matthew Miller <matthew@millerti.me>
2020-07-14 22:10:32 -0700

    Update page title and meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="A collection of TypeScript-first libraries for simpler WebAuthn integration. Supports modern browsers and Node.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
