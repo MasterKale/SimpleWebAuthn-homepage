0650d1e114657b7ee56e47e0fd7d07d2f620091a
Matthew Miller <matthew@millerti.me>
2023-12-30 22:10:12 -0800

    Alphabetize docs sidebar

--- sidebars.js
+++ sidebars.js
@@ -5,24 +5,24 @@ module.exports = {
       'simplewebauthn/philosophy',
     ],
     Packages: [
-      'packages/server',
       'packages/browser',
+      'packages/server',
       'packages/types',
     ],
     'Advanced Guides': [
-      'advanced/passkeys',
-      'advanced/supported-devices',
       'advanced/browser-quirks',
       'advanced/example-project',
       'advanced/fido-conformance',
+      'advanced/passkeys',
+      'advanced/supported-devices',
       {
         type: 'category',
         label: '@simplewebauthn/server',
         description: 'Advanced functionality specific to @simplewebauthn/server',
         items: [
           'advanced/server/metadata-service',
-          'advanced/server/settings-service',
           'advanced/server/secure-payment-confirmation',
+          'advanced/server/settings-service',
         ],
       },
     ],
