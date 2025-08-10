96b4140341c774f6ff43d66b6b5dea22a06bf735
Matthew Miller <matthew@millerti.me>
2025-08-10 11:34:06 -0700

    Add initial PRF docs

--- sidebars.js
+++ sidebars.js
@@ -14,6 +14,7 @@ module.exports = {
       'advanced/example-project',
       'advanced/fido-conformance',
       'advanced/passkeys',
+      'advanced/prf',
       'advanced/supported-devices',
       {
         type: 'category',
